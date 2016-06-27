48652c61a75f800f91a834430e552bc1e1a3931a
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2016-06-27 20:32:52 +0800

    Modify feature.py, use some simple strong feature and add url as key into feature file.

--- src/feature.py
+++ src/feature.py
@@ -4,7 +4,7 @@
 from re import compile
 from json import dumps
 from codecs import open
-from numpy import array, zeros
+from numpy import array, zeros, sum, mean, var
 from jieba import cut
 from argparse import ArgumentParser
 from progressbar import ProgressBar
@@ -27,6 +27,9 @@ def main():
 
     url_re = compile("(?<=<url:)[^>]*(?=>)")
     content_re = compile("(?<=<content:)[^>]*(?=>)")
+    image_re = compile("(?<=\[img\])[^\[\]]+(?=\[/img\])")
+    image_sub = compile("\[img\][^\[\]]+\[/img\]")
+    br_split = compile("\[br\]")
 
     tag_dict = {"0": 0, "1":1, "2":1, "3":0, "4":1, "5":1}
 
@@ -74,24 +77,44 @@ def main():
                 continue
             content = result.group(0)
 
+            image_list = []
+            result = image_re.findall(content)
+            for group in result:
+                image_list.append(group)
+
+            content = image_sub.sub("", content)
+            sentence_list = [sentence for sentence in br_split.split(content) if not (len(sentence) == 0 or sentence.isspace())]
+
             if url in url_dict:
-                url_dict[url]["content"] = content
+                url_dict[url]["image_list"] = image_list
+                url_dict[url]["sentence_list"] = sentence_list
     
     with open(data_file, 'w') as fd:
         progress = ProgressBar(maxval = len(url_dict)).start()
         counter = 0
         for url in url_dict:
-            if "content" in url_dict[url]:
-                #print url_dict[url]["tag"], url_dict[url]["content"]
-                seg_list = cut(url_dict[url]["content"])
-                feature = [0] * len(correlate_list)
-                for seg in seg_list:
-                    if seg.encode("utf-8") in correlate_set:
-                        feature[correlate_dict[seg.encode("utf-8")]] += 1
-                #feature_list.append(feature)
-                #label_list.append(tag_dict[url_dict[url]["tag"]])
+            if "image_list" in url_dict[url] and "sentence_list" in url_dict[url]:
+                image_list = url_dict[url]["image_list"]
+                sentence_list = url_dict[url]["sentence_list"]
+                
+                # sentence_count
+                sentence_count = len(sentence_list)
+
+                # word_count & word_mean & word_var
+                length_list = []
+                for sentence in sentence_list:
+                    seg_list = cut(sentence)
+                    length_list.append(sum([1 for seg in seg_list]))
+                word_count = sum(length_list)
+                word_mean = mean(length_list)
+                word_var = var(length_list)
+
+                # image_count
+                image_count = len(image_list)
+                
+                feature = [sentence_count, word_count, word_mean, word_var, image_count]
                 data = feature + [tag_dict[url_dict[url]["tag"]]]
-                fd.write("%s\n" % " ".join([str(val) for val in data]))
+                fd.write("%s\t%s\n" % (url, " ".join([str(val) for val in data])))
             counter += 1
             progress.update(counter)
         progress.finish()
